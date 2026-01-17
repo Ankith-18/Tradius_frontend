35c800479765ce937648552389664b3b0b823049
Ankith-18 <ankitkr3012005@gmail.com>
2026-01-17 13:33:00 +0530

    vite and tailwind configurte

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+       <h1>Welcome to Tradius</h1>
+      </div>
+   
+    </>
+  )
+}
+
+export default App
